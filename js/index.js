b7da340a07174b44e8e418730bff67792b81ac14
chloepei867 <pei.zhiy@northeastern.edu>
2025-09-15 19:40:36 -0700

    completed mobile layout

---
+++ js/index.js
@@ -0,0 +1,24 @@
+// Write custom JavaScript here.
+// You may ignore this file and delete if if JavaScript is not required for your challenge.
+
+const imgList = document.getElementById("img-list");
+
+//render product image list
+fetch("./data/product-images.json")
+  .then((res) => res.json())
+  .then((data) => {
+    let str = "";
+    data.forEach((item) => {
+      str += `
+      <div class="w-20 h-[120px] flex-shrink-0">
+              <img
+                class="w-full h-full object-cover rounded-md"
+                src=${item.image_url}
+                alt="product"
+              />
+            </div>
+      `;
+    });
+    imgList.innerHTML = str;
+  })
+  .catch((err) => console.error(err));
